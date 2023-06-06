e95f8bd089527797091367348eed674f3f36c347
exp-ext <ext77@yandex.ru>
2023-06-06 19:03:51 +0300

    test workflow

--- ml/src/model.py
+++ ml/src/model.py
@@ -1,9 +1,18 @@
 import pickle
 
+import numpy as np
 from config import BASE_DIR
 
 
-def predict(x):
+def predict_1_item(x):
+    """
+    The function attempts to load a pre-trained machine learning model from
+    a pickle file located in the "trained_model" directory. If the model
+    cannot be loaded, an exception is raised. The function then uses the
+    loaded model to make a prediction on the input "x" and returns the result.
+    If an error occurs during the prediction process, the function returns an
+    error message.
+    """
     try:
         with open(f'{BASE_DIR}/src/trained_model/model1.pcl', 'rb') as fid:
             model = pickle.load(fid)
@@ -12,6 +21,37 @@ def predict(x):
 
     try:
         result = model.predict(x)
+    except Exception as error:
+        return f'Predict: {error}'
+
+    return result
+
+
+def predict_many_items(x):
+    """
+    The function takes an input "x" and loads two trained models from files.
+    It then uses the first model to make a prediction on "x" and appends the
+    result to "x". Finally, it uses the second model to make a prediction on
+    the updated "x". If any errors occur during the process, it returns an
+    error message. Otherwise, it returns the predicted result.
+    """
+    try:
+        with open(f'{BASE_DIR}/src/trained_model/model2.pcl', 'rb') as fid:
+            model2 = pickle.load(fid)
+
+        with open(f'{BASE_DIR}/src/trained_model/model3.pcl', 'rb') as fid:
+            model3 = pickle.load(fid)
+
+    except OSError:
+        raise Exception('Failed to open the model file')
+
+    try:
+        result_1 = model2.predict(x)
+
+        x_1 = np.column_stack((x, result_1))
+
+        result = model3.predict(x_1)
+
     except Exception as error:
         return f'Predict: {error}'
 
--- ml/src/routers.py
+++ ml/src/routers.py
@@ -1,8 +1,15 @@
+import pandas as pd
 from fastapi import APIRouter, status
-from model import predict
+from model import predict_1_item, predict_many_items
 from schemas import Order
 
 router = APIRouter()
+
+IMPOTENT_CARGOTYPE = [
+    'cargotype_20', 'cargotype_210', 'cargotype_303',
+    'cargotype_310', 'cargotype_315', 'cargotype_340',
+    'cargotype_360', 'cargotype_910'
+]
 
 
 @router.get('/health')
@@ -13,10 +20,37 @@ def health():
 @router.post('/pack')
 def get_prediction(request: Order):
     items = []
-    for el in request.items:
-        if 340 not in el.cargotype:
-            items.append(el.dict())
-    y = predict(items)
+    df_items = pd.DataFrame(
+        items, columns=[
+            'sku', 'count', 'a', 'b', 'c', 'goods_wght', 'cargotype'
+        ]
+    )
+    for column in IMPOTENT_CARGOTYPE:
+        df_items[column] = ""
+
+    cargotypes = [20, 910, 315, 340, 310, 360, 210, 303]
+
+    for item in range(len(cargotypes)):
+        column_name = 'cargotype_' + str(cargotypes[item])
+        amount = cargotypes[item]
+        df_items[column_name] = df_items.apply(
+            lambda x: 1 if amount in x['cargotype'] else 0, axis=1
+        )
+
+    df_items = df_items.rename(
+        columns={'count': 'count_items'}
+    )
+    df_items = df_items.drop('cargotype', axis=1)
+
+    df_items['pack_volume'] = (
+        df_items['a'] * df_items['b'] * df_items['c']
+    ).astype('int')
+
+    if len(items) == 1:
+        y = predict_1_item(df_items.drop('count_items'))
+    else:
+        y = predict_many_items(df_items)
+
     return {
         'orderId': request.orderkey,
         'package': y,
