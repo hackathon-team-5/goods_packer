18e0538aa83c1302ec87ed33e47e3c8ffbdb7dff
exp-ext <ext77@yandex.ru>
2023-06-14 10:05:47 +0300

    ml router

--- ml/src/model.py
+++ ml/src/model.py
@@ -6,14 +6,6 @@ from fastapi import status
 
 
 def predict_1_item(x):
-    """
-    The function attempts to load a pre-trained machine learning model from
-    a pickle file located in the "trained_model" directory. If the model
-    cannot be loaded, an exception is raised. The function then uses the
-    loaded model to make a prediction on the input "x" and returns the result.
-    If an error occurs during the prediction process, the function returns an
-    error message.
-    """
     try:
         with open(f'{BASE_DIR}/src/trained_model/model1.pcl', 'rb') as fid:
             model = pickle.load(fid)
@@ -33,13 +25,6 @@ def predict_1_item(x):
 
 
 def predict_many_items(x):
-    """
-    The function takes an input "x" and loads two trained models from files.
-    It then uses the first model to make a prediction on "x" and appends the
-    result to "x". Finally, it uses the second model to make a prediction on
-    the updated "x". If any errors occur during the process, it returns an
-    error message. Otherwise, it returns the predicted result.
-    """
     try:
         with open(f'{BASE_DIR}/src/trained_model/model2.pcl', 'rb') as fid:
             model2 = pickle.load(fid)
