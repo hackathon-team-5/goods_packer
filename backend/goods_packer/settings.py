58340b7eb9b441503d8d637552415b18fdd74337
exp-ext <ext77@yandex.ru>
2023-06-19 21:22:48 +0300

    frontend

--- backend/goods_packer/settings.py
+++ backend/goods_packer/settings.py
@@ -70,6 +70,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'core.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'goods_packer.urls'
